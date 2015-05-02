ff44c0d2f5b4bca1f15163f85702533c38126e9a
fractalf <post@alfmarius.net>
2015-05-02 20:58:08 +0200

    Improvements

--- ghost/entrypoint.sh
+++ ghost/entrypoint.sh
@@ -1,22 +1,17 @@
 #!/bin/sh
 
-# Set $GHOST to the folder for ghost content and config
-# Usage:
-#   1) docker run -d -e GHOST=/data -v /host/ghost/data:/data <image>
-#   2) docker run -d -e GHOST=/data --volumes-from dataContainer <image>
+if [ $GHOST ]; then
 
-if [ ! -z ${GHOST+x} ]; then
-
-    if [ -d "$GHOST/content" ]; then
-        mv "content" "content-default"
-        ln -s "$GHOST/content" "content"
+    if [ -d $GHOST/content ]; then
+        mv content content-default
+        ln -s $GHOST/content content
     fi
 
-    if [ -f "$GHOST/config.js" ]; then
-        mv "config.js" "config.js-default"
-        ln -s "$GHOST/config.js" "config.js"
+    if [ -f $GHOST/config.js ]; then
+        mv config.js config.js-default
+        ln -s $GHOST/config.js config.js
     fi
 
 fi
 
-exec "$@"
+exec $@
